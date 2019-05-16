f29fb68487ba134989af322b433631aeedff6ffb
lewis <lewistmcmillan@gmail.com>
2019-05-16 21:35:07 +0100

    added nelder mead proof images

--- fluro/images/2dupdate.py
+++ fluro/images/2dupdate.py
@@ -24,11 +24,24 @@ def banana(x, y):
     b = 100.
     return (a - x)**2 + b * (y - x**2)**2
 
+#sum(100.0*(x[1:] - x[:-1]**2.0)**2.0 + (1 - x[:-1])**2.0
+
+
+def himmelblau(x, y):
+    return (x**2 + y - 11)**2 + (x + y**2 - 7)**2
+    # return sum((x[1:]**2 + x[:-1] - 11)**2 + (x[1:] + x[:-1]**2 - 7)**2)
+
+
+def sphere(x):
+    return sum(x[1:]**2 + x[:-1]**2)
+
+
 npts = 201
-x, y = np.mgrid[-2:2:npts * 1j, -1:3:npts * 1j]
+x, y = np.mgrid[-5:5:npts * 1j, -5:5:npts * 1j]
 x.shape = (npts**2)
 y.shape = (npts**2)
-z = rosen(np.vstack((x, y)))
+# z = himmelblau(np.vstack((x, y)))
+z = himmelblau(x, y)
 x.shape = (npts, npts)
 y.shape = (npts, npts)
 z.shape = (npts, npts)
@@ -36,13 +49,9 @@ z.shape = (npts, npts)
 
 def animate(i):
 
-    x, y, fit = np.loadtxt("bannana-thesis.dat", unpack=True)
+    x, y, fit = np.loadtxt("himmelblau-thesis.dat", unpack=True)
 
-    xavg = 0.
-    yavg = 0.
-    zavg = 0.
-    k = 0
-    for i in range(0, len(x) - 3, 3):
+    for i in range(0, len(x), 3):
         p1 = [x[i], y[i]]  # seperate points
         p2 = [x[i + 1], y[i + 1]]
         p3 = [x[i + 2], y[i + 2]]
@@ -51,15 +60,18 @@ def animate(i):
         xs = [p1[0], p2[0], p3[0], p1[0], p2[0], p3[0]]
         ys = [p1[1], p2[1], p3[1], p1[1], p2[1], p3[1]]
 
-        if i == len(x) - 3:
-            ax.plot(xs, ys, color="black")
-        else:
-            ax.plot(xs, ys)
-ax.scatter(1., 1., marker="o", s=50, color="black", zorder=10)
+        ax.plot(xs, ys)
+
+
+ax.scatter(3., 2., marker="o", s=50, color="black", zorder=10)
+ax.scatter(-2.805118, 3.283186, marker="o", s=50, color="black", zorder=10)
+ax.scatter(-3.779310, -3.283186, marker="o", s=50, color="black", zorder=10)
+ax.scatter(3.584458, -1.848126, marker="o", s=50, color="black", zorder=10)
+
 animate(0)
-levels = np.logspace(0.35, 3.2, 5)
+levels = np.logspace(0.00001, 5.5, 10.)
 ax.contour(x, y, z, levels, colors="k")
 
 fig.set_size_inches(width, height)
-fig.savefig('bannana.pdf')
+fig.savefig('himmelblau.pdf')
 plt.show()

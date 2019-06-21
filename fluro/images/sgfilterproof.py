cb96804caf2a54b3238b230c545d1a599bfd7e55
Lewis McMillan <lewistmcmillan@gmail.com>
2019-06-21 13:28:22 +0100

    added two figures

---
+++ fluro/images/sgfilterproof.py
@@ -0,0 +1,46 @@
+import matplotlib as mpl
+mpl.use('pdf')
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+width = 6.510  # inches
+height = width / 1.618
+
+plt.rc('font', family='serif')
+plt.rc('text', usetex=True)
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=8)
+plt.rc('axes', labelsize=12)
+
+fig, axs = plt.subplots(1, 2)
+plt.subplots_adjust(top=.99, bottom=.1, right=.99, left=.1, wspace=0.0)
+
+files = ["fluro_out.dat", "fluro_outl.dat"]
+for file in files:
+    x = np.loadtxt(file, unpack=True)
+    x /= np.amax(x)
+    axs[0].plot(x, label=file)
+
+files = ["fluro_outf.dat", "fluro_outlf.dat"]
+for file in files:
+    x = np.loadtxt(file, unpack=True)
+    if "lf" in file:
+        lab = "Many photons"
+    else:
+        lab = "Less photons"
+    axs[1].plot(x, label=lab)
+
+for ax in axs:
+    ax.set_xlim(310, 760)
+    ax.set_ylim(0., 1.01)
+axs[0].set_ylabel("Signal/arb.")
+axs[0].set_xlabel("Wavelength/nm")
+axs[1].set_xlabel("Wavelength/nm")
+axs[1].set_yticks([])
+
+axs[1].legend()
+fig.set_size_inches(width, height)
+fig.savefig('sgfilter-prrof.pdf')
+
+plt.show()

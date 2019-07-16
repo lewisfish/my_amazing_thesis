b2923330e8ac6c122b42a481488cf91349ce424b
Lewis McMillan <lewistmcmillan@gmail.com>
2019-07-16 11:39:14 +0100

    added to plan and bits to salvo.tex

--- fluro/images/eemdatabase/plotdatabase.py
+++ fluro/images/eemdatabase/plotdatabase.py
@@ -1,30 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import glob as gb
 
-file = "Elastin_eem.txt"
-file2 = "nadh-emit.dat"
-x, y = np.loadtxt(file2, unpack=True)
+files = gb.glob("*.txt")
+files.sort()
+cor = ["FAD_eem.txt"]
+for file in files:
+    eem = []
+    excite = []
 
-eem = []
-excite = []
+    with open(file, "r") as infile:
+        lines = infile.readlines()
+    emit = [float(i) for i in lines[0].split(" ")]
 
-with open(file, "r") as infile:
-    lines = infile.readlines()
-emit = [float(i) for i in lines[0].split(" ")]
+    eem = np.loadtxt(file, skiprows=1)
+    eem = np.array(eem)
+    eem = eem.reshape((28, 100))
+    excite = eem[:, 0][::-1]
+    eem = eem[:, 1:]
 
-eem = np.loadtxt(file, skiprows=1)
-eem = np.array(eem)
-eem = eem.reshape((28, 100))
-excite = eem[:, 0][::-1]
-eem = eem[:, 1:]
+    # plt.imshow(eem, extent=[emit[0], emit[-1], excite[0], excite[-1]], interpolation="gaussian")
 
-# plt.imshow(eem, extent=[emit[0], emit[-1], excite[0], excite[-1]], interpolation="gaussian")
-val = len(excite) - np.searchsorted(excite, 365) + 1
-x1 = eem[val - 1, :]
-x2 = eem[val, :]
-prop = (x2 + x1) / 2
-plt.plot(emit, prop / np.amax(prop))
-plt.plot(emit, x2 / np.amax(x2))
-plt.plot(emit, x1 / np.amax(x1))
-# plt.plot(x, y)
+    val = len(excite) - np.searchsorted(excite, 365) + 1
+    x1 = eem[val - 1, :]
+    x2 = eem[val, :]
+    prop = (x2 + x1) / 2
+    if file in cor:
+        prop /= np.amax(prop)
+        plt.plot(emit, prop, label=file[:-8])
+plt.legend()
 plt.show()
--- fluro/images/plot-bestnm.py
+++ fluro/images/plot-bestnm.py
@@ -1,14 +1,43 @@
+import matplotlib as mpl
+mpl.use('pdf')
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
-files = ["best_fluro.dat", "target-10d.dat"]
+
+width = 6.510  # inches
+height = width / 1.618
+
+plt.rc('font', family='serif', serif='Times')
+plt.rc('text', usetex=True)
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=8)
+plt.rc('axes', labelsize=12)
+
+fig, ax = plt.subplots()
+fig.subplots_adjust(left=.075, bottom=.1, right=.99, top=.98)
+
+files = ["thesis_out.dat", "fad_out.dat", "nadh_out.dat", "collagen_out.dat"]
 for file in files:
     x = np.loadtxt(file, unpack=True)
-    plt.plot(x, label=file)
+    name = file[0].upper() + file[1:-8]
+    xs = signal.savgol_filter(x, 101, 2)
+    if "thesis" in file:
+        maxval = np.amax(xs)
+        name = "NM output"
+    if "ad" in name:
+        name = name.upper()
+    ax.plot(xs / maxval, label=name)
 
-x = np.loadtxt("target-8.dat", unpack=True)
-plt.plot(x, linestyle="--", color="k", zorder=0, label="full target")
-plt.ylim(0, 1.)
 
-plt.legend()
+x = np.loadtxt("target-10d.dat", unpack=True)
+ax.plot(x, linestyle="--", color="k", zorder=0, label="Target")
+
+ax.set_xlabel("Wavelength/nm")
+ax.set_ylabel("Fluorescence intensity/AU")
+ax.set_ylim(0, 1.)
+
+ax.legend()
+fig.set_size_inches(width, height)
+fig.savefig('nm-bestfit.pdf')
 plt.show()

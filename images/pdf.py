0b18d58f6189bdb7452fa6a25d41239a3522f5e6
Lewis McMillan <lewistmcmillan@gmail.com>
2019-07-17 17:03:12 +0100

    added to intro and conclusion

--- images/pdf.py
+++ images/pdf.py
@@ -2,39 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def trapInt(wave, flux):
-    summ = 0.
+def getCDF(wave, flux):
     cdf = np.zeros(len(wave) - 1)
-    for i in range(0, len(wave) - 1):
-        summ += 0.5 * (flux[i+1] + flux[i]) * (wave[i + 1] - wave[i])
-        cdf[i] = summ
+
+    for j in range(0, 37):
+        summ = 0.
+        for i in range(0, j):
+            summ += 0.5 * (flux[i + 1] + flux[i]) * (wave[i + 1] - wave[i])
+        cdf[j] = summ
+    # summ = 0.
+    # cdf = np.zeros(len(wave) - 1)
+    # for i in range(0, len(wave) - 1):
+    #     summ += 0.5 * (flux[i + 1] + flux[i]) * (wave[i + 1] - wave[i])
+    #     cdf[i] = summ
     return cdf / cdf[-1]
 
 
-def getwaves(ws, c, l, xis):
-    waves = np.zeros(len(l))
-    for j, i in enumerate(l):
-        waves[j] = ws[i] + (ws[i + 1] - ws[i]) * ((xis[j] - c[j]) / (c[j + 1] - c[j]))
-    return waves
+def interp(targ, c, pos, i):
+    return float(targ[i] + (targ[i + 1] - targ[i]) * ((pos - c[i]) / (c[i + 1] - c[i])))
 
 
 spectrum = "solar.dat"
-f = open(spectrum, "r")
-lines = f.readlines()
-x = []
-y = []
-for line in lines:
-    data = line.split(",")
-    x.append(float(data[0]))
-    y.append(float(data[1]))
+x, y = np.loadtxt(spectrum, unpack=True, delimiter=",")
 
+cdf = getCDF(x, y)
+waves = []
+flux = []
+for i in range(1, 10000):
+    xi = np.random.rand(1)
+    loc = int(np.searchsorted(cdf, xi) - 1)
+    wave = interp(x, cdf, xi, loc)
+    waves.append(wave)
+    flux.append(interp(y, x, wave, loc))
 
-cdf = trapInt(x, y)
-xis = np.random.rand(10)
-locs = np.searchsorted(cdf, xis)
-locs -= 1
-
-waves = getwaves(x, cdf, locs, xis)
-print(waves)
-plt.plot(x, y)
+plt.plot(x, y, label="Original")
+plt.scatter(waves, flux, label="")
+plt.legend()
 plt.show()
